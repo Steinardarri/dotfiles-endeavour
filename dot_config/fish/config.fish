d07a007176d1f4f29032618b5574f89e63aba419
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-07-13 23:38:34 +0000

    minn fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,7 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+source ~/.profile
+
+atuin init fish | source
+zoxide init fish --cmd cd | source
+starship init fish | source
+pay-respects fish --alias | source
