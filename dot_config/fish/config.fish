4da8e65fc54ad702bc7b787b8ee9aefe1984d0a9
Steinar Darri Þorgilsson <steinar@steinardth.xyz>
2025-07-13 23:53:15 +0000

    auka fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,8 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 source ~/.profile
 
+fish_config theme choose "kanagawa"
+
 atuin init fish | source
 zoxide init fish --cmd cd | source
 starship init fish | source
